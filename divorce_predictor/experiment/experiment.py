34e7f1c3de16da7bd6f9af0b8ebcedea06a80221
ricoms <ricardosavii@gmail.com>
2021-11-13 08:59:30 -0300

    testing out GaussianNB

--- divorce_predictor/experiment/experiment.py
+++ divorce_predictor/experiment/experiment.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 from joblib import dump
-from sklearn.dummy import DummyClassifier
+from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import cross_validate
 
 from divorce_predictor.base import BaseExperiment
@@ -17,7 +17,7 @@ class Experiment(BaseExperiment):
 
     def setup(self):
         self.scoring = ["roc_auc", "balanced_accuracy"]
-        self.model = DummyClassifier()
+        self.model = GaussianNB()
 
     def run(self):
         self.cv_results = cross_validate(
